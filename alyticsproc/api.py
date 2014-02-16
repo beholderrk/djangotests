0fb97fa86e038dbbdf5316481d70e0d865632648
Rodion Shergin <beholderrk@gmail.com>
2014-02-17 02:48:28 +0400

    api для сайта

---
+++ alyticsproc/api.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.models import User
+from tastypie.authentication import BasicAuthentication
+from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from tastypie import fields
+from tastypie.validation import FormValidation
+from django import forms
+from .models import DataItem, DataSet, ExecHistory
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        allowed_methods = ['get']
+        authentication = BasicAuthentication()
+
+
+class DataSetAuthorization(Authorization):
+    def allowed_objs(self, object_list, bundle):
+        allowed = []
+
+        # Since they may not all be saved, iterate over them.
+        for obj in object_list:
+            if obj.user == bundle.request.user:
+                allowed.append(obj)
+
+        return allowed
+
+    def read_list(self, object_list, bundle):
+        return object_list.filter(user=bundle.request.user)
+
+    def read_detail(self, object_list, bundle):
+        return bundle.request.user == bundle.obj.user
+
+    def create_list(self, object_list, bundle):
+        return object_list
+
+    def create_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def update_list(self, object_list, bundle):
+        return self.allowed_objs(object_list, bundle)
+
+    def update_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def delete_list(self, object_list, bundle):
+        # Sorry user, no deletes for you!
+        return self.allowed_objs(object_list, bundle)
+
+    def delete_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+
+class DataSetResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+
+    class Meta:
+        queryset = DataSet.objects.all()
+        resource_name = 'dataset'
+        authentication = BasicAuthentication()
+        authorization = DataSetAuthorization()
--- alyticsproc/urls.py
+++ alyticsproc/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url, patterns, include
+from .api import DataSetResource, UserResource
+from tastypie.api import Api
+
+
+v1_api = Api(api_name='v1')
+v1_api.register(DataSetResource())
+v1_api.register(UserResource())
+
 
 urlpatterns = patterns('alyticsproc.views',
     url(r'^$', 'index', name='index'),
     url(r'^save-test-data$', 'save_testdata', name='save_testdata'),
     url(r'^start-processing/$', 'start_processing', name='start_processing'),
+
+    url(r'api/', include(v1_api.urls)),
 )
